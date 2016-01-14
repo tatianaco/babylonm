0128d74e9ccde20f33e780d1d58b7c0c7ffeb936
Tatiana Cooper <tatianaco@mellanox.com>
2016-01-14 19:44:00 +0200

    js files removed - 2

--- js/cordova_plugins.js
+++ js/cordova_plugins.js
@@ -16,23 +16,8 @@ module.exports = [
         "clobbers": [
             "cordova.plugins.barcodeScanner"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
-        "id": "cordova-plugin-console.console",
-        "pluginId": "cordova-plugin-console",
-        "clobbers": [
-            "console"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-console/www/logger.js",
-        "id": "cordova-plugin-console.logger",
-        "pluginId": "cordova-plugin-console",
-        "clobbers": [
-            "cordova.logger"
-        ]
     }
+  
 ];
 module.exports.metadata = 
 // TOP OF METADATA
